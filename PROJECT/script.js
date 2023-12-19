fbf71b63b42519bce01452f4f1231fe18e8a081a
Sam Toki <SamToki@outlook.com>
2023-12-19 21:43:09 +0800

    Following main page v1.64 update.

--- PROJECT/script.js
+++ PROJECT/script.js
@@ -266,7 +266,7 @@
 				"Completion",
 				"计时完成！<br />" +
 				"从 " + ReadText("Label_TimerDashboardStartTime") + " 至 " + ReadText("Label_TimerDashboardEndTime") + "。<br />" +
-				"设定时长 " + Math.floor(Timer.Duration / 60000) + "分" + Math.floor(Timer.Duration % 60000 / 1000).toString().padStart(2, "0") + "秒，实际时长 " + Math.floor((Timer.EndTime - Timer.StartTime) / 60000) + "分" + Math.floor((Timer.EndTime - Timer.StartTime) % 60000 / 1000).toString().padStart(2, "0") + "秒。",
+				"设定时长" + Math.floor(Timer.Duration / 60000) + "分" + Math.floor(Timer.Duration % 60000 / 1000).toString().padStart(2, "0") + "秒，实际时长" + Math.floor((Timer.EndTime - Timer.StartTime) / 60000) + "分" + Math.floor((Timer.EndTime - Timer.StartTime) % 60000 / 1000).toString().padStart(2, "0") + "秒。",
 				"", "", "确定");
 			PlayAudio("Audio_SoundRingtone");
 			ResetTimer();
