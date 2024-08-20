9999eea3449f6071188ec6293de3014a6a43a18a
Sam Toki <SamToki@outlook.com>
2024-08-20 23:52:00 +0800

    Following main page v5.14 update.

--- PROJECT/scripts/script.js
+++ PROJECT/scripts/script.js
@@ -123,6 +123,12 @@
 	function RefreshSystem() {
 		// Settings
 			// Display
+			if(window.matchMedia("(prefers-contrast: more)").matches == false) {
+				ChangeDisabled("Combobox_SettingsTheme", false);
+			} else {
+				System.Display.Theme = "HighContrast";
+				ChangeDisabled("Combobox_SettingsTheme", true);
+			}
 			ChangeValue("Combobox_SettingsTheme", System.Display.Theme);
 			switch(System.Display.Theme) {
 				case "Auto":
@@ -191,6 +197,12 @@
 				Show("Topbar");
 			} else {
 				Hide("Topbar");
+			}
+			if(window.matchMedia("(prefers-reduced-motion: reduce)").matches == false) {
+				ChangeDisabled("Combobox_SettingsAnim", false);
+			} else {
+				System.Display.Anim = 0;
+				ChangeDisabled("Combobox_SettingsAnim", true);
 			}
 			ChangeValue("Combobox_SettingsAnim", System.Display.Anim);
 			ChangeAnimOverall(System.Display.Anim);
