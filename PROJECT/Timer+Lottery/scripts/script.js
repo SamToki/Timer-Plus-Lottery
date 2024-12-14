106337476fc55e161859a2dd63d79dfc90bef4be
Sam Toki <SamToki@outlook.com>
2024-12-14 12:01:56 +0800

    Minor script optimization.

--- PROJECT/Timer+Lottery/scripts/script.js
+++ PROJECT/Timer+Lottery/scripts/script.js
@@ -711,7 +711,6 @@
 	// Dialog
 	function AnswerDialog(Selector) {
 		let DialogEvent = Interaction.Dialog[Interaction.Dialog.length - 1].Event;
-		Interaction.Dialog.splice(Interaction.Dialog.length - 1, 1);
 		ShowDialog("Previous");
 		switch(DialogEvent) {
 			case "System_LanguageUnsupported":
