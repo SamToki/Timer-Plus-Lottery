6a83d86412b47c49da38c2ef69d23e97cd9f3cc2
Sam Toki <SamToki@outlook.com>
2025-08-20 13:27:30 +0800

    Following home page v6.23 update.

--- PROJECT/Timer+Lottery/scripts/script.js
+++ PROJECT/Timer+Lottery/scripts/script.js
@@ -263,6 +263,7 @@
 				case "Genshin":
 				case "GenshinFurina":
 				case "GenshinNahida":
+				case "SilentWitch":
 					ChangeCursorOverall("url(../cursors/" + System.Display.Cursor + ".cur), auto");
 					break;
 				default:
