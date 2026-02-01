405543849f8fc4595f94faba6a9ff74af6843d77
Sam Toki <SamToki@outlook.com>
2026-02-01 23:28:21 +0800

    Following home page v9.04 update.

--- PROJECT/Timer+Lottery/scripts/script.js
+++ PROJECT/Timer+Lottery/scripts/script.js
@@ -6,7 +6,6 @@
 	// Declare variables
 	"use strict";
 		// Unsaved
-		const CurrentVersion = 3.13;
 		var Timer0 = {
 			Status: {
 				IsTimeUp: false
@@ -98,16 +97,6 @@
 				AlertSystemError("The value of System.I18n.Language \"" + System.I18n.Language + "\" in function Load is invalid.");
 				break;
 		}
-		if(System.Version.TimerPlusLottery != undefined && System0.RepairedUserData != "") {
-			ShowDialog("System_MajorUpdateDetected",
-				"Info",
-				"检测到影响用户数据的版本更新。若您继续使用旧版本的用户数据，则有可能发生兼容性问题。敬请留意。<br />" +
-				"<br />" +
-				"版本：v" + System.Version.TimerPlusLottery.toFixed(2) + " → v" + CurrentVersion.toFixed(2) + "<br />" +
-				"已修复用户数据：" + System0.RepairedUserData,
-				"", "", "", "确定");
-		}
-		System.Version.TimerPlusLottery = CurrentVersion;
 		if(localStorage.TimerPlusLottery_Subsystem != undefined) {
 			Subsystem = JSON.parse(localStorage.getItem("TimerPlusLottery_Subsystem"));
 		}
@@ -133,8 +122,8 @@
 					const ServiceWorkerInstallation = ServiceWorkerRegistration.installing;
 					ServiceWorkerInstallation.addEventListener("statechange", function() {
 						if(ServiceWorkerInstallation.state == "installed" && navigator.serviceWorker.controller != null) {
-							Show("Label_HelpPWANewVersionReady");
-							ShowDialog("System_PWANewVersionReady",
+							Show("Label_HelpNewVersionReady");
+							ShowDialog("System_NewVersionReady",
 								"Info",
 								"新版本已就绪，将在下次启动时生效。",
 								"", "", "", "确定");
@@ -151,8 +140,8 @@
 					case ServiceWorkerRegistration.waiting != null:
 						ChangeText("Label_SettingsPWAServiceWorkerRegistration", "等待更新");
 						AddClass("Label_SettingsPWAServiceWorkerRegistration", "GreenText");
-						Show("Label_HelpPWANewVersionReady");
-						ShowDialog("System_PWANewVersionReady",
+						Show("Label_HelpNewVersionReady");
+						ShowDialog("System_NewVersionReady",
 							"Info",
 							"新版本已就绪，将在下次启动时生效。",
 							"", "", "", "确定");
@@ -743,8 +732,7 @@
 		ShowDialog("Previous");
 		switch(DialogEvent) {
 			case "System_LanguageUnsupported":
-			case "System_MajorUpdateDetected":
-			case "System_PWANewVersionReady":
+			case "System_NewVersionReady":
 			case "System_RefreshingWebpage":
 			case "System_JSONStringInvalid":
 			case "System_UserDataExported":
